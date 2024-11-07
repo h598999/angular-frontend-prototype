0c29953e9022d10cd8f03afc69c6f6483dca0cf7
h598999 <h598999@stud.hvl.no>
2024-11-07 12:07:45 +0100

    init commit on angular prototype

--- feed-app-prototype/src/app/app.component.ts
+++ feed-app-prototype/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ShowPollsComponent } from './show-polls/show-polls.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ShowPollsComponent], // Import ShowPollsComponent here
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // Corrected styleUrls
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'feed-app-prototype';
 }
-
